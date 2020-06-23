092957f14f9412314d5204f3912061962b00a581
y-c-mby <211525852@mby.co.il>
2020-06-23 11:32:38 +0300

    commit number 2

--- vector.h
+++ vector.h
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cmath>
 template <typename T>
 class Vector{
 public:
@@ -8,37 +9,68 @@ public:
     ~Vector();
     Vector& operator=(const Vector& v);
     size_t size() const;
+    size_t max_size() const;
+    size_t capacity() const;
 
 private:
+    Vector& init(Vector & v,size_t size);
     T* m_vec;
     size_t m_size;
     size_t m_capacity;
 
 };
 template <typename T>
-Vector::Vector():m_size(0),m_capacity(0),m_vec(NULL){
+Vector<T>::Vector():m_size(0),m_capacity(0),m_vec(NULL){
 }
 template <typename T>
-Vector::Vector(size_t size, T value):m_vec(NULL) ,m_size(size),m_capacity(size) {
+Vector<T>::Vector(size_t size, T value):m_vec(NULL) ,m_size(size),m_capacity(size) {
     m_vec=new T[m_size];
     for (int i = 0; i <size ; ++i) {
         m_vec[i] = value;
     }
 }
 template <typename T>
-Vector::Vector(Vector &v):m_vec(NULL),m_size(v.size()),m_capacity(v.getCapacity()) {
+Vector<T>& Vector<T>::init(Vector<T> &v, size_t size) {
     m_vec =new T[m_capacity];
     for (int i = 0; i < m_size; ++i) {
         m_vec[i]=v[i];
     }
 }
 template <typename T>
-Vector::~Vector() {
+Vector<T>::Vector(Vector<T> &v):m_vec(NULL),m_size(v.size()),m_capacity(v.capacity()) {
+    m_vec =new T[m_capacity];
+    for (int i = 0; i < m_size; ++i) {
+        m_vec[i]=v[i];
+    }
+}
+template <typename T>
+Vector<T>::~Vector() {
     delete [] m_vec;
     m_vec=NULL;
 }
 template <typename T>
-Vector& Vector::operator=(const Vector &v) {
+Vector<T>& Vector<T>::operator=(const Vector<T> &v) {
+    m_capacity=v.capacity();
+    m_size=v.size();
+    T* orig = m_vec;
+    m_vec =new T[m_capacity];
+    for (int i = 0; i < m_size; ++i) {
+        m_vec[i]=v[i];
+    }
+    delete orig;
+    return *this;
+}
 
+template <typename T>
+size_t Vector<T>::size() const {
+    return m_size;
+}
+template <typename T>
+size_t Vector<T>::max_size() const {
+    return (pow(2, sizeof(T)))-1;
+}
+template <typename T>
+size_t Vector<T>::capacity() const {
+    return m_capacity;
 }
 
